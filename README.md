fecc5bee06fb9d2fc1cfdd2fe7d0caa88dbdd2ad
Carlos Torres <j.carlos.torres@me.com>
2025-01-02 17:34:19 -0800

    Add dummy commands for the base CLI interface

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # todo-list
 Small app to practice Rust
+
+## How to run it
+
+```sh
+cargo run -- <command> <parameter 1> <parameter 2> ... <parameter 3>
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,67 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    List {
+        #[arg(short, long, default_value_t = false, conflicts_with = "pending")]
+        completed: bool,
+
+        #[arg(short, long, default_value_t = false, conflicts_with = "completed")]
+        pending: bool,
+    },
+    Add {
+        #[arg(short, long)]
+        item: String,
+    },
+    Remove {
+        #[arg(short, long)]
+        id: i64,
+    },
+    Complete {
+        #[arg(short, long)]
+        id: i64,
+    },
+    Clean,
+}
+
+fn list(completed: bool, pending: bool) {
+    println!(
+        "Running List command for completed '{}' and pending '{}' filters.",
+        completed, pending
+    )
+}
+
+fn add(item: String) {
+    println!("Running Add command with item '{}'", item)
+}
+
+fn remove(id: i64) {
+    println!("Running Remove command for item '{}'", id)
+}
+
+fn complete(id: i64) {
+    println!("Running Complete command for item '{}'", id)
+}
+
+fn clean() {
+    println!("Running Clean command.")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.command {
+        Commands::List { completed, pending } => list(completed, pending),
+        Commands::Add { item } => add(item),
+        Commands::Remove { id } => remove(id),
+        Commands::Complete { id } => complete(id),
+        Commands::Clean => clean(),
+    }
 }
